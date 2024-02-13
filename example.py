07866f39d8d572f0f4d952770b8a4e3979773aa3
srsorensen <144022552+srsorensen@users.noreply.github.com>
2024-02-13 09:58:09 +0100

    Update example.py

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-path = "D:/Top_Down_Method/2023-12-06_wavelength_sweep_IRFilter_945nm_optimized_1_ST3_width_1350nm_TM_2/wavelength_sweep_IRFilter_945nm_optimized_1_ST3_width_1350nm_TM_2_926.7nm.bmp"
+path = "C:/Users/simon/PycharmProjects/SPA/wavelength_sweep_IRFilter_945nm_optimized_ST3_width_1350nm_TM_1_935.4nm.bmp"
 image = Image.open(path)
 
 
@@ -18,15 +18,13 @@ image = spa.rotate_image(image,"flip")
 #spa.set_um_per_pixel(input_point,output_point)
 
 left_indent = 200
-right_indent = 105
+right_indent = 200
 waveguide_sum_width = 80
 IQR_neighbor_removal = 5
 sum_width = 80
 
-right_indent_opt = spa.find_optimal_right_indent(image, left_indent,waveguide_sum_width ,IQR_neighbor_removal)
-left_indent_opt = spa.find_optimal_left_indent(image, right_indent_opt ,waveguide_sum_width ,IQR_neighbor_removal)
-sum_width_opt = spa.find_optimal_waveguide_sum_width(image, left_indent_opt, right_indent_opt,IQR_neighbor_removal)
-print(right_indent_opt)
-print(left_indent_opt)
-#print("The optimal left indent is: ", left_indent_opt, " The optimal right indent is: ", right_indent_opt, "and the optimal sum width is: ", sum_width_opt)
-print(spa.analyze_image(image,left_indent_opt,right_indent_opt ,sum_width_opt,IQR_neighbor_removal))
+left_indent_opt = spa.optimize_parameter("left indent", image, left_indent, right_indent, waveguide_sum_width, IQR_neighbor_removal)
+right_indent_opt = spa.optimize_parameter("right indent", image, left_indent_opt,right_indent, waveguide_sum_width ,IQR_neighbor_removal)
+sum_width_opt = spa.optimize_parameter("sum width", image, left_indent_opt,right_indent_opt, waveguide_sum_width ,IQR_neighbor_removal)
+print("The optimal left indent is: ", left_indent_opt, " The optimal right indent is: ", right_indent_opt, "and the optimal sum width is: ", sum_width_opt)
+print(spa.analyze_image(image,left_indent_opt,right_indent_opt,sum_width_opt,IQR_neighbor_removal))
