54e2b637ec16a215aa4217905af6ff7bf7ce23e2
srsorensen <144022552+srsorensen@users.noreply.github.com>
2024-03-20 13:51:32 +0100

    Update waveguide_finder.py

--- waveguide_finder.py
+++ waveguide_finder.py
@@ -21,10 +21,11 @@ from scipy.fft import ifft2, fftshift, fft2, ifftshift
 from scipy.signal import find_peaks, savgol_filter, convolve, convolve2d
 
 
-def mean_image_intensity(image, disk_size, q1=0, q3=1):
+def mean_image_intensity(image):
+    disk_size = 20
     mean_disk = disk(disk_size)
 
-    mean_image = (rank.mean_percentile(image, footprint=mean_disk, p0=q1, p1=q3))
+    mean_image = (rank.mean_percentile(image, footprint=mean_disk, p0=0, p1=1))
     return mean_image
 
 
@@ -35,15 +36,34 @@ def find_path(bw_image, start, end):
 
 
 
-def find_input_and_output(indent_list, image):
-    input_indent_start = int(image.shape[1] * indent_list[0])
-    input_indent_end = int(image.shape[1] * indent_list[1])
+def find_input_and_output():
+    image = util.img_as_float(imread(path))
+    # image = (rotate(image,180,resize=True))
 
-    output_indent_start = int(image.shape[1] * indent_list[2])
-    output_indent_end = int(image.shape[1] * indent_list[3])
+    # plt.figure(figsize=(10, 6))
+    # plt.title("Histogram of channels")
+    # plt.hist(image[:, :, 2].ravel(), bins=256, histtype='step', color='blue')
+    # plt.hist(image[:, :, 1].ravel(), bins=256, histtype='step', color='green')
+    # plt.hist(image[:, :, 0].ravel(), bins=256, histtype='step', color='red')
+    # plt.yscale("log")
 
-    input_index = image[:, input_indent_start:input_indent_end] > 0.02
-    imshow(input_index)
+    # plt.figure(figsize=(10, 6))
+    # plt.title("Original Image")
+    # imshow(image)
+
+    # grey_image = rgb2gray(image)
+    grey_image = image[:, :, 2]
+
+    indent_list = [0, 0.05, 0.9, 1]
+
+    input_indent_start = int(grey_image.shape[1] * indent_list[0])
+    input_indent_end = int(grey_image.shape[1] * indent_list[1])
+
+    output_indent_start = int(grey_image.shape[1] * indent_list[2])
+    output_indent_end = int(grey_image.shape[1] * indent_list[3])
+
+    input_index = grey_image[:, input_indent_start:input_indent_end] > 0.02
+    #imshow(input_index)
 
     cy, cx = ndi.center_of_mass(input_index)
 
@@ -52,13 +72,13 @@ def find_input_and_output(indent_list, image):
     input_point = (int(cx), int(cy))
     # print(input_point)
 
-    output_index = image[:, output_indent_start:output_indent_end] > 0.02
+    output_index = grey_image[:, output_indent_start:output_indent_end] > 0.02
     cy, cx = ndi.center_of_mass(output_index)
-    cx = image.shape[1] - cx
+    cx = grey_image.shape[1] - cx
 
     output_point = (int(cx), int(cy))
 
-    return input_point, output_point
+    return input_point, output_point, grey_image
 
 
 def um_per_pixel(point1, point2, distance):
@@ -99,6 +119,7 @@ def remove_outliers_IQR(x, data, blocks, num_neighbors):
 
 from math import isclose
 def opt_indent(parameter,x_iqr,y_savgol):
+    font_size = 16
     indent = np.arange(0, 800, 20)
     dI = indent[1] - indent[0]
     alpha_dB_i = []
@@ -200,7 +221,11 @@ def opt_indent(parameter,x_iqr,y_savgol):
     return ideal_indent
 
 
-def path_finder(threshold,in_point,out_point):
+def path_finder(threshold,in_point,out_point,grey_image):
+    sobel_h = ndi.sobel(grey_image, 0)
+    sobel_v = ndi.sobel(grey_image, 1)
+    magnitude = np.sqrt(sobel_h ** 2 + sobel_v ** 2)
+    font_size = 16
     path_length = []
     threshold = np.round(np.linspace(0.01, threshold, 10), 2)
 
@@ -241,39 +266,81 @@ def path_finder(threshold,in_point,out_point):
     plt.imshow(magnitude, cmap="turbo")
     return x_path, y_path
 
+
+def spiral_fit(x_iqr,y_iqr,y_savgol,x,y_raw,l,r):
+    font_size = 16
+    x_iqr = x_iqr[l:-r]
+    y_iqr = y_iqr[l:-r]
+    y_savgol = y_savgol[l:-r]
+    x = x[l:-r]
+    y_raw = y_raw[l:-r]
+
+    fit_x = x_iqr
+    intensity_values = y_savgol
+
+    initial_guess = [25, 0.0006, np.min(y_iqr)]
+    fit_parameters, fit_parameters_cov_var_matrix, infodict, mesg, ier, = curve_fit(exponential_function_offset, x_iqr,
+                                                                                    y_iqr, p0=initial_guess,
+                                                                                    full_output=True,
+                                                                                    maxfev=5000)  # sigma=weights, absolute_sigma=True
+    fit_outlier = exponential_function_offset(x_iqr, fit_parameters[0], fit_parameters[1], fit_parameters[2])
+
+    residuals = y_iqr - exponential_function_offset(fit_x, *fit_parameters)
+    ss_res = np.sum(residuals ** 2)
+    ss_tot = np.sum((y_iqr - np.mean(y_iqr)) ** 2)
+    r_squared_raw = 1 - (ss_res / ss_tot)
+
+    alpha_dB_raw = 10 * np.log10(np.exp(fit_parameters[1] * 10))
+    alpha_dB_raw_variance = 10 * np.log10(np.exp(np.sqrt(fit_parameters_cov_var_matrix[1, 1]) * 10))
+
+    initial_guess = [25, 0.0006, np.min(y_raw)]
+    fit_parameters, fit_parameters_cov_var_matrix, infodict, mesg, ier, = curve_fit(exponential_function_offset, x,
+                                                                                    y_raw, p0=initial_guess,
+                                                                                    full_output=True,
+                                                                                    maxfev=5000)  # sigma=weights, absolute_sigma=True
+    fit_r = exponential_function_offset(x, fit_parameters[0], fit_parameters[1], fit_parameters[2])
+    fit_guess = exponential_function_offset(x, *initial_guess)
+    residuals = fit_r - y_raw
+    mean_squared_error = np.mean(residuals ** 2)
+
+    residuals = y_raw - exponential_function_offset(x, *fit_parameters)
+    ss_res = np.sum(residuals ** 2)
+    ss_tot = np.sum((y_raw - np.mean(y_raw)) ** 2)
+    r_squared_r = 1 - (ss_res / ss_tot)
+
+    alpha_dB_r = 10 * np.log10(np.exp(fit_parameters[1] * 10))
+    alpha_dB_r_variance = 10 * np.log10(np.exp(np.sqrt(fit_parameters_cov_var_matrix[1, 1]) * 10))
+
+    plt.figure()
+    plt.plot(x_iqr, fit_outlier, color="#E69F00", linestyle="-", linewidth=3,
+             label=f"Fit to outlier corrected data\n {alpha_dB_raw:.1f}$\pm${alpha_dB_raw_variance:.1f} dB/cm, R\u00b2: {r_squared_raw:.2f}")  # ,
+    plt.plot(x, fit_r, color="g", linestyle="-", linewidth=3,
+             label=f"Fit to raw data\n {alpha_dB_r:.1f}$\pm${alpha_dB_r_variance:.1f} dB/cm, R\u00b2: {r_squared_r:.2f}")  # ,
+    plt.scatter(x, y_raw, color="#0072B2", s=1.5, label="Raw data")
+    plt.scatter(x_iqr, y_iqr, color="#000000", s=1.5, label="Outlier corrected data")
+    lgnd = plt.legend(fontsize=14, scatterpoints=1, frameon=False)
+    lgnd.legendHandles[2]._sizes = [30]
+    lgnd.legendHandles[2].set_alpha(1)
+    lgnd.legendHandles[3]._sizes = [30]
+    lgnd.legendHandles[3].set_alpha(1)
+    plt.xlabel('Propagation length [mm]', fontsize=font_size)
+    plt.ylabel('Mean intensity [a.u.]', fontsize=font_size)
+    plt.xlim([min(x), max(x)])
+    plt.ylim([min(y_raw), max(y_raw) + 5])
+    plt.xticks(fontsize=font_size)
+    plt.yticks(fontsize=font_size)
+    plt.show()
+
+    return alpha_dB_raw, alpha_dB_raw_variance, r_squared_raw, alpha_dB_r, alpha_dB_r_variance,r_squared_r
+
+
 path = "C:/Users/au617007/PycharmProjects/Open-Source-Toolbox-for-Rapid-and-Accurate-Photographic-Characterization-of-Optical-Propagation/2023-09-08_10_24_16_651_w31_1.3_waveguide2_spiral.png"
 
-image = util.img_as_float(imread(path))
-# image = (rotate(image,180,resize=True))
 
-#plt.figure(figsize=(10, 6))
-#plt.title("Histogram of channels")
-#plt.hist(image[:, :, 2].ravel(), bins=256, histtype='step', color='blue')
-#plt.hist(image[:, :, 1].ravel(), bins=256, histtype='step', color='green')
-#plt.hist(image[:, :, 0].ravel(), bins=256, histtype='step', color='red')
-#plt.yscale("log")
-
-#plt.figure(figsize=(10, 6))
-#plt.title("Original Image")
-#imshow(image)
-
-# grey_image = rgb2gray(image)
-grey_image = image[:, :, 2]
-
-indent_list = [0, 0.05, 0.9, 1]
-in_point, out_point = find_input_and_output(indent_list, grey_image)
+in_point, out_point, grey_image = find_input_and_output()
 
 out_point = (out_point[0], out_point[1] + 210) #(1886,1208)
 in_point = (in_point[0], in_point[1] - 15)  # -80
-
-#plt.figure(figsize=(10, 6))
-#plt.title("Grayscale Image with input and output and max pixel values removed")
-#plt.plot(*in_point, "ro")
-#plt.plot(*out_point, "ro")
-#imshow(grey_image)
-
-#plt.plot(*point1, "bo")
-#plt.plot(*point2, "bo")
 
 point1 = np.array((in_point[0], in_point[1]))
 point2 = np.array((in_point[0], out_point[1]))#1985
@@ -281,21 +348,11 @@ point2 = np.array((in_point[0], out_point[1]))#1985
 distance_um = 1.399#1102  # Measured in klayout
 mum_per_pixel = um_per_pixel(point1, point2, distance_um)
 
-sobel_h = ndi.sobel(grey_image, 0)
-sobel_v = ndi.sobel(grey_image, 1)
-magnitude = np.sqrt(sobel_h ** 2 + sobel_v ** 2)
 
-font_size = 16
 
-x_path, y_path = path_finder(0.1,in_point,out_point)
+x_path, y_path = path_finder(0.1,in_point,out_point,grey_image)
 
-disk_size = 20
-row = 20
-col = 3
-q1 = 0.00
-q3 = 1
-
-mean_image = mean_image_intensity(grey_image, disk_size)
+mean_image = mean_image_intensity(grey_image)
 
 y_raw = mean_image[y_path, x_path]
 
@@ -308,106 +365,10 @@ x_iqr, y_iqr, indexes = remove_outliers_IQR(x, y_raw, 10, 1)
 
 y_savgol = savgol_filter(y_iqr, 2000, 1)
 
-
+font_size = 16
 intensity_values = mean_image[y_path, x_path]
-
-plt.figure()
-# plt.title("Mean of intensity values as a function of distance, with background")
-plt.scatter(x, y_raw, color="k", s=1, alpha=0.4)
-plt.scatter(x_iqr, y_iqr, s=2, color="b", alpha=0.7)
-plt.plot(x_iqr, y_savgol, "r-", linewidth=3)
-# plt.plot(background_intensity)
-plt.xlabel('x Length [um]', fontsize=font_size)
-plt.ylabel('Mean pixel intensity [a.u.]', fontsize=font_size)
-lgnd = plt.legend(["Raw data", "Outlier corrected", "Smoothed data"], fontsize=font_size, scatterpoints=1,
-                  frameon=False)
-lgnd.legendHandles[0]._sizes = [30]
-lgnd.legendHandles[0].set_alpha(1)
-lgnd.legendHandles[1].set_alpha(1)
-lgnd.legendHandles[1]._sizes = [30]
-plt.xlim([0, 8000])
-plt.ylim([0, 110])
-plt.show()
 
 l = opt_indent("left indent",x_iqr,y_savgol)
 r = opt_indent("right indent",x_iqr,y_savgol)
 
-x_iqr = x_iqr[l:-r]
-y_iqr = y_iqr[l:-r]
-y_savgol = y_savgol[l:-r]
-x = x[l:-r]
-y_raw = y_raw[l:-r]
-
-fit_x = x_iqr
-intensity_values = y_savgol
-
-initial_guess = [25, 0.0006, np.min(intensity_values)]
-fit_parameters, fit_parameters_cov_var_matrix, infodict, mesg, ier, = curve_fit(exponential_function_offset, fit_x,
-                                                                                intensity_values, p0=initial_guess,
-                                                                                full_output=True,
-                                                                                maxfev=5000)  # sigma=weights, absolute_sigma=True
-fit = exponential_function_offset(fit_x, fit_parameters[0], fit_parameters[1], fit_parameters[2])
-fit_guess = exponential_function_offset(fit_x, *initial_guess)
-residuals = fit - intensity_values
-mean_squared_error = np.mean(residuals ** 2)
-
-residuals = intensity_values - exponential_function_offset(fit_x, *fit_parameters)
-ss_res = np.sum(residuals ** 2)
-ss_tot = np.sum((intensity_values - np.mean(intensity_values)) ** 2)
-r_squared = 1 - (ss_res / ss_tot)
-
-alpha_dB = 10 * np.log10(np.exp(fit_parameters[1] * 10))
-alpha_dB_variance = 10 * np.log10(np.exp(np.sqrt(fit_parameters_cov_var_matrix[1, 1]) * 10))
-
-initial_guess = [25, 0.0006, np.min(y_iqr)]
-fit_parameters, fit_parameters_cov_var_matrix, infodict, mesg, ier, = curve_fit(exponential_function_offset, x_iqr,
-                                                                                y_iqr, p0=initial_guess,
-                                                                                full_output=True,maxfev=5000)  # sigma=weights, absolute_sigma=True
-fit_raw = exponential_function_offset(x_iqr, fit_parameters[0], fit_parameters[1], fit_parameters[2])
-fit_guess = exponential_function_offset(fit_x, *initial_guess)
-residuals = fit - intensity_values
-mean_squared_error = np.mean(residuals ** 2)
-
-residuals = y_iqr - exponential_function_offset(fit_x, *fit_parameters)
-ss_res = np.sum(residuals ** 2)
-ss_tot = np.sum((y_iqr - np.mean(y_iqr)) ** 2)
-r_squared_raw = 1 - (ss_res / ss_tot)
-
-alpha_dB_raw = 10 * np.log10(np.exp(fit_parameters[1] * 10))
-alpha_dB_raw_variance = 10 * np.log10(np.exp(np.sqrt(fit_parameters_cov_var_matrix[1, 1]) * 10))
-
-initial_guess = [25, 0.0006, np.min(y_raw)]
-fit_parameters, fit_parameters_cov_var_matrix, infodict, mesg, ier, = curve_fit(exponential_function_offset, x,
-                                                                                y_raw, p0=initial_guess,
-                                                                                full_output=True,maxfev=5000)  # sigma=weights, absolute_sigma=True
-fit_r = exponential_function_offset(x, fit_parameters[0], fit_parameters[1], fit_parameters[2])
-fit_guess = exponential_function_offset(x, *initial_guess)
-residuals = fit_r - y_raw
-mean_squared_error = np.mean(residuals ** 2)
-
-residuals = y_raw - exponential_function_offset(x, *fit_parameters)
-ss_res = np.sum(residuals ** 2)
-ss_tot = np.sum((y_raw - np.mean(y_raw)) ** 2)
-r_squared_r = 1 - (ss_res / ss_tot)
-
-alpha_dB_r = 10 * np.log10(np.exp(fit_parameters[1] * 10))
-alpha_dB_r_variance = 10 * np.log10(np.exp(np.sqrt(fit_parameters_cov_var_matrix[1, 1]) * 10))
-
-plt.figure()
-
-plt.plot(x_iqr, fit_raw, color="#E69F00",linestyle="-", linewidth=3,label=f"Fit to outlier corrected data\n {alpha_dB_raw:.1f}$\pm${alpha_dB_raw_variance:.1f} dB/cm, R\u00b2: {r_squared_raw:.2f}")  # ,
-plt.plot(x, fit_r, color="g",linestyle="-", linewidth=3,label=f"Fit to raw data\n {alpha_dB_r:.1f}$\pm${alpha_dB_r_variance:.1f} dB/cm, R\u00b2: {r_squared_r:.2f}")  # ,
-plt.scatter(x, y_raw, color="#0072B2", s=1.5, label="Raw data")
-plt.scatter(x_iqr, y_iqr, color="#000000", s=1.5,label="Outlier corrected data")
-lgnd = plt.legend(fontsize=14, scatterpoints=1, frameon=False)
-lgnd.legendHandles[2]._sizes = [30]
-lgnd.legendHandles[2].set_alpha(1)
-lgnd.legendHandles[3]._sizes = [30]
-lgnd.legendHandles[3].set_alpha(1)
-plt.xlabel('Propagation length [mm]', fontsize=font_size)
-plt.ylabel('Mean intensity [a.u.]', fontsize=font_size)
-plt.xlim([min(x), max(x)])
-plt.ylim([min(y_raw), max(y_raw)+5])
-plt.xticks(fontsize=font_size)
-plt.yticks(fontsize=font_size)
-plt.show()
+alpha_dB_raw, alpha_dB_raw_variance, r_squared_raw, alpha_dB_r, alpha_dB_r_variance,r_squared_r  = spiral_fit(x_iqr,y_iqr,y_savgol,x,y_raw,l,r)
