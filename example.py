f26f54a0458519733596a143735c83ce2267da9e
srsorensen <144022552+srsorensen@users.noreply.github.com>
2024-04-04 16:16:12 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,30 +1,28 @@
 from PIL import Image
-import sys
-sys.path.append("C:/Users/shd-PhotonicLab/PycharmProjects/SPA")
 from SPA import SPA
-import numpy as np
-import matplotlib.pyplot as plt
+import os
 
 
-path = "C:/Users/simon/PycharmProjects/SPA/wavelength_sweep_IRFilter_945nm_optimized_ST3_width_1350nm_TM_1_935.4nm.bmp"
-image = Image.open(path)
-
-
+spa_directory = os.getcwd()
 spa = SPA(True,4870) #set flag to False to turn off plotting
 
+#Load spiral image
+image_path = spa_directory + "/" + "spiral_sample_data.png"
+
+#Distance per pixel
+distance_per_pixel = 1.399 # µm
+
+#Bool used to indicate whether the script should optimize parameters or not.
+optimize_parameter = False
+
+#Fitting to spiral
+alpha_dB_outlier, alpha_dB_outlier_variance, r_squared_outlier, alpha_dB_raw, alpha_dB_raw_variance,r_squared_raw = spa.spiral_waveguide(image_path,distance_per_pixel,optimize_parameter)
+
+#Load straight waveguide
+image_path = spa_directory + "/" + "wavelength_sweep_IRFilter_945nm_optimized_ST3_width_1350nm_TM_1_928.7nm.bmp"
+image = Image.open(image_path)
+
+#Flip to make sure input is on the left
 image = spa.rotate_image(image,"flip")
 
-#spa.manual_input_and_output(input_point, output_point)
-#spa.set_um_per_pixel(input_point,output_point)
-
-left_indent = 200
-right_indent = 200
-waveguide_sum_width = 80
-IQR_neighbor_removal = 5
-sum_width = 80
-
-left_indent_opt = spa.optimize_parameter("left indent", image, left_indent, right_indent, waveguide_sum_width, IQR_neighbor_removal)
-right_indent_opt = spa.optimize_parameter("right indent", image, left_indent_opt,right_indent, waveguide_sum_width ,IQR_neighbor_removal)
-sum_width_opt = spa.optimize_parameter("sum width", image, left_indent_opt,right_indent_opt, waveguide_sum_width ,IQR_neighbor_removal)
-print("The optimal left indent is: ", left_indent_opt, " The optimal right indent is: ", right_indent_opt, "and the optimal sum width is: ", sum_width_opt)
-print(spa.analyze_image(image,left_indent_opt,right_indent_opt,sum_width_opt,IQR_neighbor_removal))
+print(spa.straight_waveguide(image,optimize_parameter))
