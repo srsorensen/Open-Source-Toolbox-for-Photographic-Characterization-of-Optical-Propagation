fdd2192689cc0f76aab09fc9ccafa9130e593138
srsorensen <144022552+srsorensen@users.noreply.github.com>
2024-08-01 21:47:27 +0200

    Update create_sweep_h5file.py

--- create_sweep_h5file.py
+++ create_sweep_h5file.py
@@ -66,54 +66,72 @@ else:
         r_squared_values = []
         alphas = []
         alpha_variances = []
-        data_x = []
-        data_y = []
         right_indent_sweep = []
         left_indent_sweep = []
         sum_width_sweep = []
+
         for file in files:
-            left_index = []
-            right_index = []
-            sum_index = []
+            invalid_left_indices = []
+            invalid_right_indices = []
+            invalid_sum_indices = []
 
-            try:
-                image = Image.open(path + key + "/" + file)
-                image = spa.rotate_image(image,"flip")
-            except:
-                print(f"PIL error at: {file}")
-                traceback.print_exc()
-                continue
+            while True:
+                try:
+                    image = Image.open(path + key + "/" + file)
+                    image = spa.rotate_image(image, "flip")
+                except:
+                    print(f"PIL error at: {file}")
+                    traceback.print_exc()
+                    break
 
-            try:
-                left_index, left_indent_opt = spa.optimize_parameter("left crop",image,left_indent,right_indent,waveguide_sum_width,IQR_neighbor_removal)
-                right_index, right_indent_opt = spa.optimize_parameter("right crop",image,left_indent,right_indent,waveguide_sum_width,IQR_neighbor_removal)
-                sum_index, sum_width_opt = spa.optimize_parameter("sum width",image,left_indent,right_indent,waveguide_sum_width,IQR_neighbor_removal)
-                alpha, rsquared, alpha_variance = spa.analyze_image(image, left_indent_opt, right_indent_opt, sum_width_opt,
-                                                          IQR_neighbor_removal)
+                try:
+                    # Optimize parameters, passing the list of invalid indices to avoid
+                    left_index, left_indent_opt = spa.optimize_parameter("left crop", image, left_indent, right_indent,
+                                                                         waveguide_sum_width, IQR_neighbor_removal,
+                                                                         invalid_left_indices)
+                    right_index, right_indent_opt = spa.optimize_parameter("right crop", image, left_indent,
+                                                                           right_indent, waveguide_sum_width,
+                                                                           IQR_neighbor_removal,
+                                                                           invalid_right_indices)
+                    sum_index, sum_width_opt = spa.optimize_parameter("sum width", image, left_indent, right_indent,
+                                                                      waveguide_sum_width, IQR_neighbor_removal,
+                                                                      invalid_sum_indices)
 
-            except:
-                traceback.print_exc()
-                print("Error")
-                continue
-            finally:
-                image.close()
+                    # Analyze the image with the optimized parameters
+                    alpha, rsquared, alpha_variance = spa.analyze_image(image, left_indent_opt, right_indent_opt,
+                                                                        sum_width_opt, IQR_neighbor_removal)
 
-            if (alpha <= 0 or alpha > 200 or np.isinf(alpha) or np.isnan(alpha)):
+                    # If the alpha value is invalid, store the indices to avoid them in the future
+                    if alpha <= 0 or alpha > 200 or np.isinf(alpha) or np.isnan(alpha):
+                        invalid_left_indices.append(left_index)
+                        invalid_right_indices.append(right_index)
+                        invalid_sum_indices.append(sum_index)
+                    else:
+                        # Valid alpha found, proceed with processing and break out of the loop
+                        print(
+                            f"{str(counter)}, left indent: {left_indent_opt}, right indent: {right_indent_opt}, sum width: {sum_width_opt}, Loss: {np.round(alpha, 1)} dB/cm")
+                        counter += 1
+                        wavelength = get_wavelength(file)
+                        alphas.append(alpha)
+                        alpha_variances.append(alpha_variance)
+                        wavelengths.append(wavelength)
+                        r_squared_values.append(rsquared)
+                        left_indent_sweep.append(left_indent_opt)
+                        right_indent_sweep.append(right_indent_opt)
+                        sum_width_sweep.append(sum_width_opt)
+                        break  # Exit while loop to move on to the next file
 
-            else:
-                #print(alpha, type(alpha))
-                print(str(counter) + ",  left indent: " + str(left_indent_opt) + ",  right indent: " + str(
-                    right_indent_opt) + ", sum width: " + str(sum_width_opt) + ",  Loss: " + str(
-                    np.round(alpha, 1)) + ' dB/cm')
-                counter = counter + 1
-                wavelength = get_wavelength(file)
-                alphas.append(alpha)
-                alpha_variances.append(alpha_variance)
-                wavelengths.append(wavelength)
-                r_squared_values.append(rsquared)
-                left_indent_sweep.append(left_indent_opt)
-                right_indent_sweep.append(right_indent_opt)
-                sum_width_sweep.append(sum_width_opt)
+                except:
+                    traceback.print_exc()
+                    print("Error")
+                    break
+                finally:
+                    image.close()
+
+            # Reset the invalid indices list for the next file
+            invalid_left_indices = []
+            invalid_right_indices = []
+            invalid_sum_indices = []
 
 
 
